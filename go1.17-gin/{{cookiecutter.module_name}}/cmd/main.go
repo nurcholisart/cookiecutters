36a16b09abd08052570bee3683441514f27b22b9
Nurcholis Art <nurcholisart@gmail.com>
2022-04-02 22:00:29 +0700

    Initialize dotenv

--- go1.17-gin/{{cookiecutter.module_name}}/cmd/main.go
+++ go1.17-gin/{{cookiecutter.module_name}}/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"github.com/joho/godotenv"
 	"{{cookiecutter.module_name}}/config"
 	"{{cookiecutter.module_name}}/helpers"
 	"{{cookiecutter.module_name}}/server"
@@ -10,6 +11,8 @@ import (
 var version string
 
 func main() {
+	_ := := godotenv.Load()
+
 	conf, err := config.DefineConfig()
 	if err != nil {
 		log.Fatalf("Failed to run server: %s", err.Error())
